cedc6bed49b2b45447cb23d0ceaaa6d98027ddf0
Adrian Barajas <a_barajas_c@hotmail.com>
2019-09-08 19:29:06 -0500

    Add simple test and test dependencies.

--- src/test/java/com/boot/AppTest.java
+++ src/test/java/com/boot/AppTest.java
@@ -1,20 +1,16 @@
 package com.boot;
 
-import static org.junit.Assert.assertTrue;
-
+import com.boot.controller.HomeController;
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
+import static org.junit.Assert.assertEquals;
+
+public class AppTest
 {
-    /**
-     * Rigorous Test :-)
-     */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testApp(){
+        HomeController homeController = new HomeController();
+        String result = homeController.home();
+        assertEquals(result,"Das Boot , Reporting for Duty!!");
     }
 }
